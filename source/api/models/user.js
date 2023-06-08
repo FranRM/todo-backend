64421cc4745bd58a4817b14b51da0d9e4c19f19b
chiscazo.paco@gmail.com <chiscazo.paco@gmail.com>
2023-06-08 11:41:11 +0200

    Add actions required to run todo-admin

--- source/api/models/user.js
+++ source/api/models/user.js
@@ -11,8 +11,11 @@ module.exports = {
 			return {
 				id          : this._id,
 				first_names : this.first_names,
-				login 		: this.login,
-				role        : this.role
+				last_names  : this.last_names,
+				role        : this.role,
+				login       : { email : this.login.email, provider : this.login.provider, state : this.login.state, date : this.login.date },
+				info        : this.info,
+				meta        : this.meta
 			}
 		},
 		using    : function (context) {
--- source/api/schemas/primary/user.js
+++ source/api/schemas/primary/user.js
@@ -2,14 +2,17 @@
 
 const fields = require('anxeb-mongoose').Fields;
 const SchemaBuilder = require('../../../middleware/schema');
-const Login = require('../common/login')
+const Login = require('../common/login');
 
 module.exports = {
 	Schema : function (params) {
 		return new SchemaBuilder(params, 'User').build((builder) => ({
 			first_names : fields.string({ required : true }),
-			login  : new Login.Schema({required:true, static:true}),
-			role  : new fields.enum({required:true}, ['client', 'admin']),
+			last_names  : fields.string({ required : true }),
+			role        : fields.enum({ required : true }, ['admin', 'client', 'broker']),
+			login       : new Login.Schema({ required : true, static : true }),
+			info        : fields.mixed(),
+			meta        : fields.mixed()
 		}));
 	}
 };
